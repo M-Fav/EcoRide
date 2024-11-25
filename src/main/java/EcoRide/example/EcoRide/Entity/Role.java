51614f9b3ba8b501fcecf383fdaa546fe8a8f341
MehdiFav <mehdi.favreau@gmail.com>
2024-11-25 22:33:41 +0100

    Creation Role Entity-Repo-Controller

---
+++ src/main/java/EcoRide/example/EcoRide/Entity/Role.java
@@ -0,0 +1,37 @@
+package EcoRide.example.EcoRide.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long roleId;
+    private String libelle;
+
+    public Role() {}
+
+    public Role(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
