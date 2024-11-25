44dced8cf52fb11b78275ecac0f5d03bbb6c32d9
mfavreau <mfavreau@cirilgroup.com>
2024-11-25 13:52:06 +0100

    initialisation du dépôt

---
+++ src/main/java/EcoRide/example/EcoRide/EcoRideApplication.java
@@ -0,0 +1,13 @@
+package EcoRide.example.EcoRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EcoRide/example/EcoRide/EcoRideApplicationTests.java
@@ -0,0 +1,13 @@
+package EcoRide.example.EcoRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
